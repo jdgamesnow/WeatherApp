2e5aed43af2b4ee1082385980a003930f5cb756b
jdgamesnow <jdgamesnow@gmail.com>
2016-03-20 17:17:53 -0400

    Basic weather map activity completed
    Ready to try integrating into another app to see any problems that may
    arise.

---
+++ WeatherSDK/jdweather/src/main/java/jeffreydelawderjr/com/jdweather/WeatherMapActivity.java
@@ -0,0 +1,121 @@
+package jeffreydelawderjr.com.jdweather;
+
+import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.util.jar.Manifest;
+
+/**
+ * Created by jdelawde on 3/20/2016.
+ */
+public class WeatherMapActivity extends FragmentActivity implements OnMapReadyCallback, ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private MapFragment mMapFragment;
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final int REQUEST_LOCATION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceBundle){
+        super.onCreate(savedInstanceBundle);
+        GoogleMapOptions mapOptions = new GoogleMapOptions();
+        mapOptions.mapType(GoogleMap.MAP_TYPE_NORMAL)
+                .compassEnabled(false)
+                .rotateGesturesEnabled(true)
+                .tiltGesturesEnabled(true);
+        mMapFragment = MapFragment.newInstance();
+
+        if (mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+    public void initializeMapFragmentWithID(int id){
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(id,mMapFragment);
+        fragmentTransaction.commit();
+        mMapFragment.getMapAsync(this);
+    }
+
+    protected void onStart(){
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult r){
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int r){
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle){
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        } else {
+            // permission has been granted, continue as usual
+            android.location.Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            Point latLong = new Point((int)location.getLatitude(), (int)location.getLongitude());
+        }
+
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if(grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission has been granted, continue as usual
+                try {
+                    android.location.Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                    Point latLong = new Point((int)location.getLatitude(), (int)location.getLongitude());
+                }catch (SecurityException e){
+
+                }
+            } else {
+                // Permission was denied or request was cancelled
+            }
+        }
+    }
+}
